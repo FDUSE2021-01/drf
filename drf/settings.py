c13be77eaa964d30111b5ee7a9c13f0f3a8581e4
TwistsOfFate <yu_fanqi@163.com>
2021-03-31 19:00:20 +0800

    Use JWT authentication

--- drf/settings.py
+++ drf/settings.py
@@ -126,5 +126,8 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2
+    'PAGE_SIZE': 2,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
