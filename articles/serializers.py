3dd85ab70e83f02018c95f14a88b1eae674d905e
TwistsOfFate <yu_fanqi@163.com>
2021-03-31 15:59:20 +0800

    Add api_view

--- articles/serializers.py
+++ articles/serializers.py
@@ -5,5 +5,5 @@ from articles.models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['id', 'title', 'content']
+        fields = ['id', 'title', 'created', 'content']
 
