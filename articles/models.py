13b06666516e35bddc99d40aea9d7c972bf2693f
xiaoas <xiaoqilin82@gmail.com>
2021-04-07 12:25:35 +0800

    init steam games info model update db with steam games info changes to db: new table articles_game

--- articles/models.py
+++ articles/models.py
@@ -16,3 +16,30 @@ class Article(models.Model):
 
     class Meta:
         ordering = ['created']
+
+class ArticlesGame(models.Model):
+    index = models.IntegerField(blank=True, null=True)
+    href = models.TextField(blank=True, null=True)
+    data_ds_appid = models.TextField(db_column='data-ds-appid', blank=True, null=True)
+    data_ds_itemkey = models.TextField(db_column='data-ds-itemkey', blank=True, null=True)
+    data_ds_tagids = models.TextField(db_column='data-ds-tagids', blank=True, null=True)
+    data_ds_crtrids = models.TextField(db_column='data-ds-crtrids', blank=True, null=True)
+    onmouseover = models.TextField(blank=True, null=True)
+    onmouseout = models.TextField(blank=True, null=True)
+    class_field = models.TextField(db_column='class', blank=True, null=True) # Field renamed because it was a Python reserved word.
+    data_search_page = models.IntegerField(db_column='data-search-page', blank=True, null=True)
+    data_gpnav = models.TextField(db_column='data-gpnav', blank=True, null=True)
+    item_name = models.TextField(blank=True, null=True)
+    platform = models.TextField(blank=True, null=True)
+    release_date = models.TextField(blank=True, null=True)
+    rating_positive_ratio = models.TextField(blank=True, null=True)
+    rating_count = models.TextField(blank=True, null=True)
+    original_pricing = models.TextField(blank=True, null=True)
+    pricing = models.TextField(blank=True, null=True)
+    data_ds_bundleid = models.FloatField(db_column='data-ds-bundleid', blank=True, null=True)
+    data_ds_bundle_data = models.TextField(db_column='data-ds-bundle-data', blank=True, null=True)
+    data_ds_descids = models.TextField(db_column='data-ds-descids', blank=True, null=True)
+    data_ds_packageid = models.FloatField(db_column='data-ds-packageid', blank=True, null=True)
+
+    class Meta:
+        db_table = 'articles_game'
